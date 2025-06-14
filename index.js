6e6bbebca8f32f981d4cad097d5286af093b104b
Nusrat Humaira <nusrathum31@gmail.com>
2025-06-14 14:34:19 +0600

    set middleWare

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const jwt = require('jsonwebtoken'); 
 const app = express();
+const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
@@ -12,6 +13,7 @@ app.use(cors({
     credentials: true
 }))
 app.use(express.json())
+app.use(cookieParser())
 
 
 
